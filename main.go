4c3e58327a9bbcb30b51b5c1d802c33f513e3f81
Nimesh Manmohanlal <nimeshjm@gmail.com>
2018-03-20 17:13:51 +0000

    SAVEPOINT

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
-func GetIdByISIN(s string) string {
-	return "F00000O4Y5"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type fund struct {
+	Id string
 }
 
-func main() {
+func GetIdByISIN(isin string) string {
+	url:= fmt.Sprintf("https://lt.morningstar.com/api/rest.svc/9vehuxllxs/security_details/%s?viewId=investmentTypeLookup&idtype=isin&languageId=en-GB&currencyId=GBP&responseViewFormat=json", isin)
 
+	httpClient := http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := httpClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	fund := make([]fund,0)
+	jsonErr := json.Unmarshal(body, &fund)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return fund[0].Id
 }
+
+func main() {}
